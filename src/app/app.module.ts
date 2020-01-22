5e621cb5a7e616736ea3404b6ab34f9b409aded9
Madona Poles <madona.poles@gmail.com>
2020-01-22 15:54:53 +0200

    7 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,10 @@ import { CartComponent } from './components/cart/cart.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LoginComponent } from './components/login/login.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
-import { MobilesComponent } from './components/mobiles/mobiles.component';
-import { LabtopsComponent } from './components/labtops/labtops.component';
-import { CameraComponent } from './components/camera/camera.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
 import {FormsModule} from '@angular/forms';
-import { SonyComponent } from './components/camera/sony/sony/sony.component';
-import { NikonComponent } from './components/camera/nikon/nikon/nikon.component';
-import { PanasonicComponent } from './components/camera/Panasonic/panasonic/panasonic.component';
-import { OlympusComponent } from './components/camera/Olympus/olympus/olympus.component';
-import { FujifilmComponent } from './components/camera/Fujifilm/fujifilm/fujifilm.component';
-import { HPComponent } from './components/labtops/HP/hp/hp.component';
-import { DellComponent } from './components/labtops/Dell/dell/dell.component';
-import { LenovoComponent } from './components/labtops/Lenovo/lenovo/lenovo.component';
-import { SamsongComponent } from './components/labtops/Samsong/samsong/samsong.component';
-import { AppleComponent } from './components/labtops/Apple/apple/apple.component';
 import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
@@ -35,22 +22,9 @@ import { CategoryComponent } from './components/category/category.component';
     SignupComponent,
     LoginComponent,
     PagenotfoundComponent,
-    MobilesComponent,
-    LabtopsComponent,
-    CameraComponent,
     HeaderComponent,
     BodyComponent,
     FooterComponent,
-    SonyComponent,
-    NikonComponent,
-    PanasonicComponent,
-    OlympusComponent,
-    FujifilmComponent,
-    HPComponent,
-    DellComponent,
-    LenovoComponent,
-    SamsongComponent,
-    AppleComponent,
     CategoryComponent
   ],
   imports: [
